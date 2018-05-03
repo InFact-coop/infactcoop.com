c80e315e921090b342ee0c28a3237f80d84708cf
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-05-03 14:42:23 +0100

    Call css import functions

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,11 +27,11 @@ module.exports = {
       resolve: `gatsby-plugin-postcss-sass`,
       options: {
         postCssPlugins: [
-          require('postcss-import'), // allows you to use @import
-          require('autoprefixer'), // adds vendor prefixes like -webkit or -moz to your css for better support
-          require('postcss-custom-media'), // can set media queries as variables
-          require('postcss-custom-properties'), // allows you to use css variables e.g. var(--my-variable)
-          require('postcss-clean'), // minifies the output css (i.e. removes all the spaces and comments)
+          require('postcss-import')(), // allows you to use @import
+          require('autoprefixer')(), // adds vendor prefixes like -webkit or -moz to your css for better support
+          require('postcss-custom-media')(), // can set media queries as variables
+          require('postcss-custom-properties')(), // allows you to use css variables e.g. var(--my-variable)
+          require('postcss-clean')(), // minifies the output css (i.e. removes all the spaces and comments)
         ],
         precision: 8,
       },
