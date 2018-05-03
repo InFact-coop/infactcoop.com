51f21b0dc5d9e0b6343f6f7ddf6ddc2e6f81448d
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-05-03 17:18:04 +0100

    Downgrade autoprefixer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
-import './index.css'
+import './index.scss'
 
 const Layout = ({ children, data }) => (
   <div>
