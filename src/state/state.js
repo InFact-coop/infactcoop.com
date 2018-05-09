e7d364e48546e3911e3d04e164cd11dc1061e3bb
Max Gerber <mgerber94@gmail.com>
2018-05-09 10:51:20 +0100

    Building successfully

--- src/state/state.js
+++ src/state/state.js
@@ -7,8 +7,9 @@ const createStore = () =>
   reduxCreateStore(
     menuReducer,
     initialState,
-    typeof window !== 'undefined' &&
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
+    typeof window !== 'undefined'
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   )
 export default createStore
