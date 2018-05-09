23ec59dd31f4fc277e234fbc805aacd0af81e40e
Max Gerber <mgerber94@gmail.com>
2018-05-09 10:26:32 +0100

    Added devtools prefix for production build

--- src/state/state.js
+++ src/state/state.js
@@ -7,6 +7,8 @@ const createStore = () =>
   reduxCreateStore(
     menuReducer,
     initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    typeof window !== 'undefined' &&
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 export default createStore
