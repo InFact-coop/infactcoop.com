577cc94e5c640ccfae12a0904739159cbb78f834
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-05-10 09:00:07 +0100

    Add FAC section

--- src/components/background-img.js
+++ src/components/background-img.js
@@ -4,6 +4,7 @@ import styled, { css } from 'styled-components'
 const BackgroundImg = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
+  background-position: center;
 
   ${({ src }) =>
     css`
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import OurApproach from '../components/our-approach'
+import FAC from '../components/fac'
 
 const IndexPage = () => (
   <div>
     <OurApproach />
+    <FAC />
   </div>
 )
 
