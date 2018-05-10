f5f247627b4d50dd22a8378128b007739cf9bc78
Max Gerber <mgerber94@gmail.com>
2018-05-10 16:25:26 +0100

    Created footer Fixes #12

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Link from 'gatsby-link'
 
 import OurApproach from '../components/our-approach'
 import FAC from '../components/fac'
+import { Footer } from '../components/footer'
 
 const IndexPage = () => (
   <div>
     <OurApproach />
     <FAC />
+    <Footer />
   </div>
 )
 
