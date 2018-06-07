8a685d58e2a9910ffac84729c2a11649236ea268
Lucy King <lucy.m.king92@gmail.com>
2018-06-07 18:14:22 +0100

    adds holding page and filters to card images

---
+++ src/pages/our-work/holding.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+import Header from '../../containers/header-work'
+import { SectionHeading, SectionTitle, Section } from '../../components/section'
+import {
+  Wrapper,
+  Picture,
+  Overlay,
+  SectionCard,
+  SectionText,
+} from '../../components/work-card'
+import { CardTitle, CardSubTitle, CardHeading } from '../../components/card'
+import {
+  WorkWrapper,
+  WorkImg,
+  WorkDescription,
+  TestimonialWrapper,
+  TestimonialPerson,
+  TestimonialText,
+  BannerImg,
+} from '../../components/work-content'
+
+import stimmy_card from '../../assets/images/card_images/stimmythings_card.svg'
+import bluecross_card from '../../assets/images/card_images/bluecross_card.png'
+import header_img from '../../assets/images/other/us_portrait_christa.jpg'
+
+const AboutWork = () => (
+  <div>
+    <Header
+      topicLine="Work in progress"
+      tagLine="Coming soon!"
+      subTagLine="We haven't made this page yet, but as you can see, we're busy working on it."
+    />
+    <div className="ma0">
+      <BannerImg src={header_img} />
+    </div>
+    <Section bg_color="white">
+      <SectionHeading mb>
+        <SectionTitle>Check these out in the meantime!</SectionTitle>
+      </SectionHeading>
+      <div className="flex-ns flex-column flex-row-ns justify-between-ns">
+        <Wrapper to="/our-work/stimmy-things">
+          <Picture src={stimmy_card}>
+            <Overlay background="linear-gradient(-225deg, rgba(0, 0, 0, 0.3) 0%, rgba(50, 45, 55, 0.3) 100%)" />
+          </Picture>
+          <SectionCard>
+            <CardHeading>
+              <CardTitle>Mental Health</CardTitle>
+            </CardHeading>
+            <CardSubTitle uppercase>Stimmy Things</CardSubTitle>
+            <SectionText>
+              Working in collaboration with young people, parents and
+              clinicians, we designed and built an app that helps young people
+              with ADHD to manage their symptoms.
+            </SectionText>
+          </SectionCard>
+        </Wrapper>
+
+        <Wrapper to="/our-work/bluecross">
+          <Picture src={bluecross_card}>
+            <Overlay background="linear-gradient(-225deg, rgba(0, 0, 0, 0.3) 0%, rgba(50, 45, 55, 0.3) 100%)" />
+          </Picture>
+          <SectionCard>
+            <CardHeading>
+              <CardTitle>Tech for Good</CardTitle>
+            </CardHeading>
+            <CardSubTitle uppercase>Blue Cross</CardSubTitle>
+            <SectionText>
+              We created an app for conscientious owners to find a loving new
+              home for their pet.
+            </SectionText>
+          </SectionCard>
+        </Wrapper>
+      </div>
+    </Section>
+  </div>
+)
+
+export default AboutWork
