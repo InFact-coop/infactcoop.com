1664d9f962aa6b61ac5144db0ca723ec50beb059
Max Gerber <mgerber94@gmail.com>
2018-06-09 23:41:43 +0100

    hover effect on logos

--- src/components/link-image.js
+++ src/components/link-image.js
@@ -4,8 +4,9 @@ import { media } from '../styles/style_utils'
 import BackgroundImg from './background-img'
 
 const LinkImage = styled(BackgroundImg.withComponent('a')).attrs({
-  className: 'h4 ma1 ma2-m ma0-l dib link pointer',
+  className: 'h4 ma1 ma2-m ma0-l dib link pointer grow',
 })`
+  transition: 0.3s !important;
   background-size: contain;
   width: 40%;
   ${media.ns`
