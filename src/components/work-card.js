03beb8489a04a26f66dc172a94c7fd142c47b3a5
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-06-11 19:40:44 +0100

    Remove bluecross copy

--- src/components/work-card.js
+++ src/components/work-card.js
@@ -58,7 +58,7 @@ const Wrapper = styled(Link).attrs({
   className:
     'relative flex-ns justify-center black no-underline db mb3 mb0-ns overflow-x-hidden overflow-y-hidden',
 })`
-  height: 99.8vh;
+  height: 100vh;
   transition: 0.5s;
 
   ${media.m`
