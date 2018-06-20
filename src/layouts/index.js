506ac2e2bd4a49743572e51a492ef624a6ee9c16
Max Gerber <mgerber94@gmail.com>
2018-06-20 16:10:07 +0100

    Favicons!

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,12 @@ import Helmet from 'react-helmet'
 import styled from 'styled-components'
 import '../styles/index.scss'
 import { Footer } from '../components/footer'
+import faviconAppleTouchIcon from '../assets/logos/favicon/favicon-original.png'
+import faviconMaskIcon from '../assets/logos/favicon/favicon.svg'
+import favicon64 from '../assets/logos/favicon/favicon_forweb/favicon-64x64.png'
+import favicon32 from '../assets/logos/favicon/favicon_forweb/favicon-32x32.png'
+import favicon16 from '../assets/logos/favicon/favicon_forweb/favicon-16x16.png'
+import faviconIco from '../assets/logos/favicon/favicon_forweb/favicon.ico'
 
 const AppWrapper = styled.div.attrs({
   className: 'bg-light-gray center',
@@ -19,7 +25,18 @@ const Layout = ({ children, data }) => {
           { name: 'description', content: 'Sample' },
           { name: 'keywords', content: 'sample, something' },
         ]}
-      />
+      >
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={faviconAppleTouchIcon}
+        />
+        <link rel="mask-icon" href={faviconMaskIcon} color="#5bbad5" />
+        <link rel="icon" type="image/png" sizes="64x64" href={favicon64} />
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+        <link rel="shortcut icon" href={faviconIco} />
+      </Helmet>
       <div className="bg-light-gray">
         <AppWrapper>
           <div>{children()}</div>
