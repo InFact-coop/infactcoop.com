0fa241ddff5f6895e038e89cfc716288a28b607b
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2018-07-02 11:50:58 +0100

    Fix FAC subtitle

--- src/components/fac.js
+++ src/components/fac.js
@@ -45,9 +45,11 @@ const FAC = () => (
       <SectionTitle>
         <div className="tc dn db-ns">We love Founders & Coders</div>
         <div className="dn-ns tc">
-          <span>We love</span>
-          <span className="pre"> Founders </span>
-          <span>& Coders</span>
+          <div className="flex flex-column">
+            <span>We love</span>
+            <span>Founders</span>
+            <span>& Coders</span>
+          </div>
         </div>
       </SectionTitle>
     </SectionHeading>
