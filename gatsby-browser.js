5163943c67e01a4eeb3b6fe3d061e634f38f428e
lucymk <lucy.m.king92@gmail.com>
2019-02-01 16:54:20 +0100

    updates gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+
 import createStore from './src/state/state'
+const store = createStore()
 
 export const wrapRootElement = ({ element }) => {
-  const store = createStore()
-
-  const ConnectedRootElement = (
-    <Provider store={store}>
-    {element}
-    </Provider>
-  )
-
-   return ConnectedRootElement
+  return <Provider store={store}>{element}</Provider>
 }
-
